81cb9afee083856f3c845276397645d74ff062d6
Yuan.Zhou <yuan.zhou@me.com>
2013-11-14 08:53:02 +0100

    Add Ruby App

---
+++ FirstFly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstFly::Application.config.secret_key_base = 'ac5f2b47cd325312ae60f9cc942f9077a8058a02bfe91ba75d6100b1d43000a45bcd5db17c442301abd9f7484b0313b1f5dfd8de05d67e01143f0f0a14687905'
